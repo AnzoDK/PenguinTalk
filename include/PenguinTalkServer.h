0745def329b4c7520bc24269780aa2b0c82a4571
Lonely <magnus.kongstad@gmail.com>
2021-05-07 15:43:41 +0200

    Solved VS2019 not being able to compile and run

--- include/PenguinTalkServer.h
+++ include/PenguinTalkServer.h
@@ -16,9 +16,9 @@ private:
     int m_port = 0;
     std::string m_host = "";
     int m_maxThreads = 0;
+    int* m_threadIDs = nullptr;
 #ifdef __linux__
     int m_fd = -1;
-    int* m_threadIDs = nullptr;
 #elif _WIN32
     SOCKET m_fd = 0;
 #endif
