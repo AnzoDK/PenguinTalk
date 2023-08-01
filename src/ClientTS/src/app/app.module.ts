33a0782de84c1dcecd9a28d3b954cf65a6010391
Anzo <antonr@live.dk>
2023-08-01 19:43:19 +0200

    added rxjs

--- src/ClientTS/src/app/app.module.ts
+++ src/ClientTS/src/app/app.module.ts
@@ -1,9 +1,14 @@
+/**
+ * This is the main module of the Angular application.
+ * It imports the necessary modules and components, and declares the AppComponent.
+ * All Angular applications must have at least one module, and it is typically named AppModule.
+ */
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {APP_BASE_HREF} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -17,3 +22,5 @@ import {APP_BASE_HREF} from '@angular/common';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
