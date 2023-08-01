833e189f11597f9daacd83ddf20e475d0bc344b3
Anzo <antonr@live.dk>
2023-08-01 21:59:39 +0200

    added new signup component

--- src/ClientTS/main.js
+++ src/ClientTS/main.js
@@ -4,29 +4,10 @@ const fs = require('fs');
 const url = require('url');
 const path = require('path');
 
-// Define the path to the index.html file
-const index = 'dist/penguin-talk-electron/index.html';
-
-// Function to patch the index.html file
-function patchIndex() {
-    // Read the file
-    let file = fs.readFileSync(index);
-    // Get the index of the '<base href="/">'
-    let _index = file.toString().indexOf('<base href="/">');
-    if (_index) {
-        // Replace '<base href="/">' with '<base href="./">'
-        let buff = file.toString();
-        let newStr = buff.replace('<base href="/">', '<base href="./">');
-        // Remove the old index.html file and write the patched version
-        fs.rmSync(index);
-        fs.writeFileSync(index, newStr);
-    }
-}
-
 // Function to create the main window
 function onReady() {
     // Apply the patch to the index.html file
-    patchIndex();
+    //patchIndex();
 
     // Create a new browser window
     let win = new BrowserWindow({width: 900, height: 6700});
